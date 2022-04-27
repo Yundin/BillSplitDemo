b9d2d25d4c777643c0fad5df28110af158bf9b2e
Vladislav Yundin <4yundin@gmail.com>
2022-04-27 18:33:35 +0400

    feat: add module for design system

--- sources/base/app/src/main/java/com/yundin/billsplitapp/MainActivity.kt
+++ sources/base/app/src/main/java/com/yundin/billsplitapp/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
 import com.yundin.billsplitapp.ui.theme.BillSplitAppTheme
 
 class MainActivity : ComponentActivity() {
